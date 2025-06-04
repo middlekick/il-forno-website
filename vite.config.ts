fb7a425c86fbafa4a7f774568daab3e263c6dbe9
Téo <teo.mikulic@gmail.com>
2025-06-04 19:08:00 +0200

    Ajout de l'attribut 'base' dans le config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/il-forno-website/", // Ajoutez cette ligne
   server: {
     host: "::",
     port: 8080,
